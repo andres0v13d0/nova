1ea540398bd4f07de19e863c48f54c0d1d7184c4
jonathan4996 <jonathan.yanzapanta@espoch.edu.ec>
2024-07-17 09:05:24 -0500

    cambios

--- Aplicacion/presentacion/public/frontend/src/components/wrapper/Wrapper.jsx
+++ Aplicacion/presentacion/public/frontend/src/components/wrapper/Wrapper.jsx
@@ -1,24 +1,25 @@
-import React from "react"
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 const Wrapper = () => {
   const data = [
     {
-      cover: <i class='fa-solid fa-id-card'></i>,
+      cover: <i className='fa-solid fa-id-card'></i>,
       title: "Pago Seguro",
       decs: "Ofrecemos precios competitivos en nuestra amplia gama de productos tecnológicos.",
     },
     {
-      cover: <i class='fa-solid fa-shield'></i>,
+      cover: <i className='fa-solid fa-shield'></i>,
       title: "Compra con Confianza",
       decs: "Garantizamos productos de calidad y soporte especializado en tecnología.",
     },
     {
-      cover: <i class='fa-solid fa-headset'></i>,
+      cover: <i className='fa-solid fa-headset'></i>,
       title: "Soporte 24/7",
       decs: "Brindamos asistencia continua para resolver tus dudas y problemas técnicos.",
     },
-  ]
+  ];
+
   return (
     <>
       <section className='wrapper background'>
@@ -32,12 +33,12 @@ const Wrapper = () => {
                 <h3>{val.title}</h3>
                 <p>{val.decs}</p>
               </div>
-            )
+            );
           })}
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Wrapper
+export default Wrapper;
